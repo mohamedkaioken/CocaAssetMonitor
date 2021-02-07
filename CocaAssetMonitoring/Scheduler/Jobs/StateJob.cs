80c74bff22a74f218e358a1a7fe4c5e95ce24625
MohamedKaioken <mohamed.m201139@gmail.com>
2021-02-08 00:09:05 +0200

    edit

---
+++ CocaAssetMonitoring/Scheduler/Jobs/StateJob.cs
@@ -0,0 +1,23 @@
+﻿using CocaAssetMonitoring.Domain.IJobs;
+using CocaAssetMonitoring.Domain.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CocaAssetMonitoring.Scheduler.Jobs
+{
+    public class StateJob : IStateJob
+    {
+        private readonly IStateService _service;
+        public StateJob(IStateService service)
+        {
+            _service = service;
+        }
+        public async Task TriggerRecurring()
+        {
+            await _service.ProcessAsync();
+        }
+    }
+}
---
+++ CocaAssetMonitoring/Services/StateService.cs
@@ -0,0 +1,28 @@
+﻿using CocaAssetMonitoring.Domain.IServices;
+using CocaAssetMonitoring.Models.BrokerInterface;
+using CocaAssetMonitoring.Models.SystemModels;
+using CocaAssetMonitoring.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocaAssetMonitoring.Services 
+{
+    public class StateService : IStateService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly InterfaceDbContext _interfaceDb;
+        private readonly BrokerDbContext _brokerDb;
+        public StateService(BrokerDbContext brokerDb, InterfaceDbContext interfaceDb, ApplicationDbContext context)
+        {
+            _context = context;
+            _interfaceDb = interfaceDb;
+            _brokerDb = brokerDb;
+        }
+
+        public async Task ProcessAsync()
+        {
+        }
+    }
+}
